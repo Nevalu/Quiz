103fc188e0e3c6e93c05db4b2c908b476f4e23f2
Neva Wood <nevalu@gmail.com>
2016-06-07 16:25:23 -0700

    will add style and content to html

--- js/scripts.js
+++ js/scripts.js
@@ -12,19 +12,19 @@ $(document).ready(function() {
 
         document.getElementById("grade").innerHTML = result;
         if (0 <= result && result <= 19) {
-            result2 = "'<a href='http://www.scp-wiki.net/scp-1370'>SCP-1370</a><br/>''<img src='img/robot.jpg' width='300'/>"
+            result2 = "'<a href='http://www.scp-wiki.net/scp-1048'>SCP-1048</a>''<br/><img src='img/teddy.jpg' width='300' />"
 
         };
         if (19 < result && result <= 39) {
-            result2 = "'<a href='http://www.scp-wiki.net/scp-1048'>SCP-1048/a>''<br/><img src='img/teddy.jpg' width='300' />"
+            result2 =  "'<a href='http://www.scp-wiki.net/scp-1370'>SCP-1370</a><img src='img/classd.jpg' width='300'/>"
         };
         if (39 < result && result <= 59) {
-            result2 = "'<a href='http://www.scp-wiki.net/scp-294'>SCP-294</a><br/><img src='img/coffeemachine.jpeg' width='300' />"
+            result2 = "'<a href='http://www.scp-wiki.net/scp-1370'>SCP-1370</a><br/>''<img src='img/robot.jpg' width='300'/>"
         };
 
 
         if (59 < result && result <= 79) {
-            result2 =  "'<a href='http://www.scp-wiki.net/scp-1370'>SCP-1370</a><img src='img/classd.jpg' width='300'/>"
+            result2 = "'<a href='http://www.scp-wiki.net/scp-294'>SCP-294</a><br/><img src='img/coffeemachine.jpeg' width='300' />"
         };
         if (79 < result && result <= 100) {
             result2 ="'<a href='http://www.scp-wiki.net/scp-SCP-829'>SCP-829</a><br/><img src='img/nailpolish.jpg' width='300' />"
