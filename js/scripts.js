04bd0e42af95ff7917b408c05a1de26a53857c37
Neva Wood <nevalu@gmail.com>
2016-06-07 12:56:13 -0700

    equation fix

--- js/scripts.js
+++ js/scripts.js
@@ -7,27 +7,25 @@ $(document).ready(function() {
         cos = parseInt(document.querySelector('input[name = "cos"]:checked').value);
         tan = parseInt(document.querySelector('input[name = "tan"]:checked').value);
 
-        result = Math.round((variable + sub + con + cos + tan) * 100 / 20);
+        result = Math.round((variable + sub + con + cos + tan) * 5);
 
         document.getElementById("grade").innerHTML = result;
-        if (0 < result && result <= 25) {
+        if (0 <= result && result <= 19) {
             result2 = "SCP-1370.<br/><img src='img/robot.jpg' width='300' />"
         };
-        if (25 < result && result <= 50) {
+        if (19 < result && result <= 39) {
             result2 = "SCP-1048.<br/><img src='img/teddy.jpg' width='300' />"
         };
-        if (50 < result && result <= 75) {
-            result2 = "SCP-294.<br/><img src='img/coffeemachine.jpg' width='300' />"
+        if (39 < result && result <= 59) {
+            result2 = "SCP-294.<br/><img src='img/coffeemachine.jpeg' width='300' />"
         };
-        if (75 < result && result <= 100) {
+        if (59 < result && result <= 79) {
             result2 = "'Ethics Committee Orientation<br /><a href=http://www.scp-wiki.net/ethics-committee-orientation src='img/classd.jpg' alt='Ethics Website'</a>' width='300' />"
         };
-        if (100 < result && result <= 125) {
+        if (79 < result && result <= 100) {
             result2 = "SCP-829<br/><img src='img/nailpolish.jpg' width='300' />"
         };
-        if (125 < result && result <= 150) {
-            result2 = "location5!<br/><img src='img/endofworld.jpg' width='300' />"
-        };
+
 
         (document).getElementById("grade2").innerHTML = result2;
 
