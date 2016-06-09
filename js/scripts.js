659ede6bc2a487963156454e029b7e9d556f2967
Neva Wood <nevalu@gmail.com>
2016-06-09 00:53:11 -0700

    finish all js html css, hurrah!

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,8 @@ $(document).ready(function() {
     console.log("line 5");
     document.getElementById("form1").onsubmit = function() {
         console.log("line 7");
+
+
         var sin = parseInt(document.querySelector('input[name = "sin"]:checked').value);
         var sub = parseInt(document.querySelector('input[name = "sub"]:checked').value);
         var con = parseInt(document.querySelector('input[name = "con"]:checked').value);
@@ -15,43 +17,47 @@ $(document).ready(function() {
         console.log("line 15");
         document.getElementById("grade").innerHTML = result;
         console.log("line 17");
-        if (0 <= result && result <= 19) {
-            result2 = "a";
-            console.log("line 20");
+        if (0 <= result && result <= 25) {
+            result2 = "Your best eerie destination is: ClassD! Click photo for info!"
+            document.getElementById('div1').style.display = 'inline';
         }
         console.log("line 22");
-        // result2 = ''<a href="http://www.scp-wiki.net/scp-1048">SCP-1048 </a>''<br/><img src='img/teddy.jpg' width='300'/>'
 
-        if (19 < result && result <= 39) {
 
-            result2 =  "b";
+        if (25 < result && result <= 50) {
+
+            result2 = "Your best eerie destination is scp-1048! Click photo for website info!"
+            document.getElementById('div2').style.display = 'inline';
         }
 
         // result2 =  "<href='http://www.scp-wiki.net/scp-1370'>SCP-1370</a><img src='img/classd.jpg' width='300'/>"
 
-        if (39 < result && result <= 59) {
-            result2 = "c";
+        if (50 < result && result <= 75) {
+            result2 = "Your best eerie destination is: the robot! spooooky..... Click the pic!"
+            document.getElementById('div3').style.display = 'inline';
         }
 
 
-        //     result2 = '<href="http://www.scp-wiki.net/scp-1370">SCP-1370</a><br/>''<img src='img/robot.jpg' width='300'/>'
+
         // };
 
 
-        if (59 < result && result <= 79) {
-            result2 = "d";
+        if (75 < result && result <= 100) {
+            result2 = "Your best eerie destination is nail polish? Weird. Click the photo for more info!"
+            document.getElementById('div4').style.display = 'inline';
         }
 
         // result2 = ''<a href='http://www.scp-wiki.net/scp-294'>SCP-294</a><br/><img src='img/coffeemachine.jpeg' width='300' />''
         // };
-        if (79 < result && result <= 100) {
-            result2 = "d";
+        if (100 < result && result <= 125) {
+            result2 = "Your best eerie destination is:79-100"
+            document.getElementById('div5').style.display = 'inline';
         }
 
 
-          //
-          //     result2 ='<href='http://www.scp-wiki.net/scp-SCP-829'>SCP-829</a><br/><img src='img/nailpolish.jpg' width='300' />'
-          // };
+        //
+        //     result2 ='<href='http://www.scp-wiki.net/scp-SCP-829'>SCP-829</a><br/><img src='img/nailpolish.jpg' width='300' />'
+        // };
 
 
         console.log("line 56");
@@ -65,4 +71,3 @@ $(document).ready(function() {
     };
 
 });
-
---
+++ quiz.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+        event.preventDefault();
+        document.getElementById("form1").onsubmit = function() {
+            variable = parseInt(document.querySelector('input[name = "variable"]:checked').value);
+            sub = parseInt(document.querySelector('input[name = "sub"]:checked').value);
+            con = parseInt(document.querySelector('input[name = "con"]:checked').value);
+            cos = parseInt(document.querySelector('input[name = "cos"]:checked').value);
+            tan = parseInt(document.querySelector('input[name = "tan"]:checked').value);
+
+            result = Math.round((variable + sub + con + cos + tan) * 5);
+
+
+            document.getElementById("grade").innerHTML = result;
+            if (0 <= result && result <= 19) {
+                result2 = function onClick(id) {
+                    id.innerHTML = "http://www.scp-wiki.net/scp-1048";
+                }
+            };
+
+            // result2 = ''<a href="http://www.scp-wiki.net/scp-1048">SCP-1048 </a>''<br/><img src='img/teddy.jpg' width='300'/>'
+            result2 == function onClick(id) {
+                id.innerHTML = "http://www.scp-wiki.net/scp-1048";
+            }
+        };
+        if (19 < result && result <= 39) {
+
+            result2 = function onClick(id) {
+                id.innerHTML = "http://www.scp-wiki.net/scp-1048";
+            }
+
+            // result2 =  "<href='http://www.scp-wiki.net/scp-1370'>SCP-1370</a><img src='img/classd.jpg' width='300'/>"
+        };
+        if (39 < result && result <= 59) {
+            result2 = function onClick(id) {
+                id.innerHTML = "http://www.scp-wiki.net/scp-1048";
+            }
+        };
+
+        //     result2 = '<href="http://www.scp-wiki.net/scp-1370">SCP-1370</a><br/>''<img src='img/robot.jpg' width='300'/>'
+        // };
+
+
+        if (59 < result && result <= 79) {
+            result2 = function onClick(id) {
+                id.innerHTML = "http://www.scp-wiki.net/scp-1048";
+            }
+        };
+        // result2 = ''<a href='http://www.scp-wiki.net/scp-294'>SCP-294</a><br/><img src='img/coffeemachine.jpeg' width='300' />''
+        // };
+        if (79 < result && result <= 100) {
+            result2 = function onClick(id) {
+                id.innerHTML = "http://www.scp-wiki.net/scp-1048";
+            }
+        };
+        //
+        //     result2 ='<href='http://www.scp-wiki.net/scp-SCP-829'>SCP-829</a><br/><img src='img/nailpolish.jpg' width='300' />'
+        // };
+
+
+        (document).getElementById("grade2").innerHTML = result2;
+
+        return false;
+
+    }
+};
